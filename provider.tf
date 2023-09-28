d9cd0783f19cc57cb313357f959b79eff064e838
Renzo Aguirre <renzo.aguirre@cjgconsultores.com>
2023-09-28 15:02:43 -0500

    adding tf  challenge

--- provider.tf
+++ provider.tf
@@ -5,11 +5,11 @@ terraform {
       version = "3.32.0"
     }
   }
-  backend "azurerm" {
-    storage_account_name = "stadv01"
-    container_name = "sta-container-dv-01"
-    key = "terraform.tfstate"
-  }
+  #backend "azurerm" {
+  #  storage_account_name = "stadv01"
+  #  container_name = "sta-container-dv-01"
+  #  key = "terraform.tfstate"
+  #}
 }
 
 provider "azurerm" {
